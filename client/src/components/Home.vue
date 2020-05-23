5e58827ad58aaa0ef8fa3647f45495f1e4babe97
Alex Gyllos <alex.gyllos@gmail.com>
2020-05-23 12:46:28 +0100

    .

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="">
-
+    <p>{{this.totalShareValue}}Hi{{totalValue}}</p>
   </div>
 </template>
 
@@ -10,7 +10,8 @@ export default {
   data() {
     return {
       userShares: {},
-      numberOfShares: null
+      numberOfShares: null,
+      totalShareValue: null
     }
   },
   mounted() {
@@ -18,9 +19,21 @@ export default {
     .then(res => res.json())
     .then(share => this.userShares['FB'] = share)
 
+    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=30min&apikey=P3TR43K4R4WKZ1YU')
+    .then(res => res.json())
+    .then(share => this.userShares['IBM'] = share)
+
     fetch('http://localhost:3000/api/shares/')
     .then(res => res.json())
     .then(data => this.numberOfShares = data[0]);
+  },
+  computed: {
+    totalValue() {
+      this.userShares.forEach((share, value) => {
+        console.log(share)
+      });
+
+    }
   }
 }
 </script>
