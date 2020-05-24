4a71f585adaa3044b6c6fcbb6263f3d2483ab840
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-05-24 16:05:10 +0100

    fixes merge conflicts

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,8 +1,11 @@
 <template lang="html">
   <div class="">
+    <div v-if="contentLoaded">
 
     <p>Total Current Shares Value: {{totalValue}}
   </p>
+
+</div>
 
   </div>
 </template>
@@ -16,6 +19,8 @@ export default {
       numberOfShares: null,
       total: null,
       latestValue: {}
+      latestValue: {},
+      contentLoaded: false
     }
   },
   mounted() {
@@ -38,10 +43,14 @@ export default {
     fetch('http://localhost:3000/api/shares/')
     .then(res => res.json())
     .then(data => this.numberOfShares = data[0]);
+
+    this.contentLoaded = true;
   },
   computed: {
     totalValue(){
       let total = 0
+      // if (this.contentLoaded === false) {return total = 0}
+      // else {
       Object.keys(this.numberOfShares).forEach((share) => {
         Object.keys(this.latestValue).forEach(key => {
           if (share === key) {
@@ -50,7 +59,15 @@ export default {
         })
       })
       return total;
-
+    }
+  },
+  methods: {
+    async getSharesData(equity){
+        const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${equity}&interval=30min&apikey=P3TR43K4R4WKZ1YU`)
+        const result = await response.json()
+        this.userShares[equity] = result;
+        let temp = result['Time Series (30min)']
+        this.latestValue[equity] = temp[Object.keys(temp).pop()];
     }
   }
 }
