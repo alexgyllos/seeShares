8a6d15ca85bb22786c41b2c9014f7861ec9c9df4
Alex Gyllos <alex.gyllos@gmail.com>
2020-05-31 22:05:45 +0100

    finishes piechart feature

---
+++ client/src/components/PieChart.vue
@@ -0,0 +1,56 @@
+<template lang="html">
+  <div id="pieChart">
+    <highcharts :options="pieChart"></highcharts>
+  </div>
+</template>
+
+<script>
+import {Chart} from 'highcharts-vue'
+
+export default {
+  name: 'PieChart',
+  props: ["pieChartData"],
+  data() {
+    return {
+      pieChart: {
+        title: {
+          text: 'My Shares'
+        },
+        type: 'pie',
+        series: [
+          {
+          name: 'Value',
+          colorByPoint: true,
+          type: 'pie',
+          data: [
+          // {
+          //   name: 'FB',
+          //   y: 30
+          // },
+          // {
+          //   name: 'IBM',
+          //   y: 70
+          // },
+          // {
+          //   name: 'BA',
+          //   y: 10
+          // }
+          ]
+        }]
+      }
+    }
+  },
+  mounted() {
+    Object.entries(this.pieChartData).forEach(([key, data]) => {
+      this.pieChart.series[0].data.push(data);
+    })
+    // this.Chart.redraw();
+  },
+  components: {
+    Chart
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
