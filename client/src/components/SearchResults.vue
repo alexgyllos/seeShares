b1e9feb7ec3c88c9d1c5c8d8bcb7e66e334a67d9
Alex Gyllos <alex.gyllos@gmail.com>
2020-06-07 21:40:12 +0100

    inits

---
+++ client/src/components/SearchResults.vue
@@ -0,0 +1,62 @@
+<template lang="html">
+  <div class="searchResults">
+
+
+    <SearchResultsItem v-if="searchResults" v-for="result in searchResults" :result="result"></SearchResultsItem>
+
+
+  </div>
+
+</template>
+
+<script>
+import SearchResultsItem from '@/components/SearchResultsItem.vue'
+
+export default {
+  name: 'SearchResults',
+  props: ['searchResults'],
+  data() {
+    return {
+      numberOfShares: 1
+    }
+  },
+  methods: {
+    bookIt(numberOfShares) {
+      console.log(numberOfShares);
+    }
+  },
+  components: {
+    SearchResultsItem
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+
+  .searchResults {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+    align-content: center;
+    align-self: center;
+    /* border: 1px solid black; */
+    margin: 20px;
+
+  }
+
+  .searchResults input {
+    width: 30px;
+    text-align: center;
+    margin: 0px 10px;
+  }
+
+  .searchResults button {
+    /* padding: 5px; */
+  }
+
+
+
+</style>
---
+++ client/src/components/SearchResultsItem.vue
@@ -0,0 +1,78 @@
+<template lang="html">
+  <div class="item">
+    <div class="resultsSymbol">
+      <p>{{result['1. symbol']}}</p>
+    </div>
+    <div class="resultsName">
+      <p>{{result['2. name']}}</p>
+    </div>
+    <div class="inputs">
+      <input type="number" min="0" name="numberOfShares" value="1" v-bind:value="numberOfShares">
+    </div>
+    <div class="buttons">
+      <button type="button" v-on:click="bookIt(numberOfShares)">Book it</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchResultsItem',
+  props: ['result']
+}
+</script>
+
+<style lang="css" scoped>
+
+.item {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.resultsSymbol {
+  display: flex;
+  /* flex-direction: column; */
+  margin-right: 100px;
+}
+
+.resultsSymbol p {
+  margin: 0px;
+  text-align: left;
+  /* border: 1px solid black; */
+}
+
+.resultsName {
+  display: flex;
+  flex-direction: column;
+  margin-right: 100px;
+}
+
+.resultsName p {
+  margin: 0px;
+  text-align: left;
+  /* border: 1px solid black; */
+}
+
+.inputs input {
+  /* margin: 15.5px 0px; */
+  width: 30px;
+  text-align: center;
+  margin: 0px 10px;
+}
+
+.inputs {
+  display: flex;
+  flex-direction: column;
+  margin: 0px;
+  /* border: 1px solid black; */
+
+}
+
+.buttons {
+  display: flex;
+  flex-direction: column;
+  /* border: 1px solid black; */
+
+}
+</style>
